5febe904b97eb2f5bffd8141230ab6080ebffd2a
Ashish <ashish@adesigner.in>
2019-09-09 01:26:52 +0530

    Revert "Initial Commit"
    This reverts commit e2d9cb0610e8a6f942cb182a3b605766f35f2526.

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function themebs_enqueue_styles() {
-
-        wp_enqueue_style( 'bootstrap4', get_stylesheet_directory_uri() . '/css/bootstrap.min.css' );
-        wp_enqueue_style( 'core', get_template_directory_uri() . '/style.css' );
-
-      }
-      add_action( 'wp_enqueue_scripts', 'themebs_enqueue_styles'); 
